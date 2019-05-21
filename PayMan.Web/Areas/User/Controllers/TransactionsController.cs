dfd0e864837719d31b6dd040107c6841d627c3dc
Teodora Dimitrova <teddy_29@abv.bg>
2019-05-22 00:42:52 +0300

    Layout was started

---
+++ PayMan.Web/Areas/User/Controllers/TransactionsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PayMan.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    //[Authorize(Roles = "User")]
+    public class TransactionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
